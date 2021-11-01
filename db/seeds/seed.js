b0c138d1413d56bc35730a3df8b598cd159659e3
praisedpern <jamesajinks@gmail.com>
2021-11-01 12:50:25 +0000

    implements seed table data insertion

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,25 +1,27 @@
 const db = require('../connection')
+const format = require('pg-format')
 
 const seed = (data) => {
     const { articleData, commentData, topicData, userData } = data
     // 1. create tables
-    return db
-        .query('DROP TABLE IF EXISTS topics, articles, users, comments;')
-        .then(() => {
-            return db.query(`CREATE TABLE topics (
-        slug VARCHAR UNIQUE NOT NULL,
+    return (
+        db
+            .query('DROP TABLE IF EXISTS topics, articles, users, comments;')
+            .then(() => {
+                return db.query(`CREATE TABLE topics (
+        slug VARCHAR PRIMARY KEY,
         description VARCHAR NOT NULL
       );`)
-        })
-        .then(() => {
-            return db.query(`CREATE TABLE users (
-                username VARCHAR(50) UNIQUE NOT NULL,
+            })
+            .then(() => {
+                return db.query(`CREATE TABLE users (
+                username VARCHAR(50) PRIMARY KEY,
                 avatar_url VARCHAR DEFAULT 'https://memegenerator.net/img/instances/72838539.jpg',
                 name VARCHAR NOT NULL 
       );`)
-        })
-        .then(() => {
-            return db.query(`CREATE TABLE articles (
+            })
+            .then(() => {
+                return db.query(`CREATE TABLE articles (
                 article_id SERIAL PRIMARY KEY,
                 title VARCHAR NOT NULL,
                 body VARCHAR NOT NULL,
@@ -28,9 +30,9 @@ const seed = (data) => {
                 author VARCHAR(50) NOT NULL REFERENCES users(username),
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       );`)
-        })
-        .then(() => {
-            return db.query(`CREATE TABLE comments (
+            })
+            .then(() => {
+                return db.query(`CREATE TABLE comments (
                 comment_id SERIAL PRIMARY KEY,
                 author VARCHAR(50) NOT NULL REFERENCES users(username),
                 article_id INT NOT NULL REFERENCES articles(article_id),
@@ -38,8 +40,70 @@ const seed = (data) => {
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                 body VARCHAR NOT NULL
       );`)
-        })
-    // 2. insert data
+            })
+            // 2. insert data
+            .then(() => {
+                const queryStr = format(
+                    `INSERT INTO topics (slug, description)
+                    VALUES
+                    %L
+                    RETURNING *;`,
+                    topicData.map((topic) => [topic.slug, topic.description])
+                )
+                return db.query(queryStr)
+            })
+            .then(() => {
+                const queryStr = format(
+                    `INSERT INTO users (
+                    username, avatar_url, name
+                    ) VALUES %L;`,
+                    userData.map((user) => [
+                        user.username,
+                        user.avatar_url,
+                        user.name,
+                    ])
+                )
+                return db.query(queryStr)
+            })
+            .then(({ rows }) => {
+                const queryStr = format(
+                    `INSERT INTO articles (
+                    title, body, votes, topic, author, created_at
+                    ) VALUES %L;`,
+                    articleData.map((article) => {
+                        return [
+                            article.title,
+                            article.body,
+                            article.votes,
+                            article.topic,
+                            article.author,
+                            article.created_at,
+                        ]
+                    })
+                )
+                return db.query(queryStr)
+            })
+            .then(() => {
+                const queryStr = format(
+                    `INSERT INTO comments (
+                    author, article_id, votes, created_at, body
+                    ) VALUES %L;`,
+                    commentData.map((comment) => {
+                        return [
+                            comment.author,
+                            comment.article_id,
+                            comment.votes,
+                            comment.created_at,
+                            comment.body,
+                        ]
+                    })
+                )
+                return db.query(queryStr)
+            })
+            .then(({rows})=> {
+                return console.log(rows)
+            })
+    )
 }
 
 module.exports = seed
