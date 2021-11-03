0c1ab547c7f296545c7ac39cb78dbcecbf7b8a19
praisedpern <jamesajinks@gmail.com>
2021-11-03 10:06:38 +0000

    refactors articles.controllers.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const testData = require('../db/data/test-data/index.js')
 const seed = require('../db/seeds/seed.js')
 const app = require('../app.js')
 const request = require('supertest')
+const { string } = require('pg-format')
 
 beforeEach(() => seed(testData))
 afterAll(() => db.end())
@@ -50,40 +51,24 @@ describe('/api/topics', () => {
 describe('/api/articles/:article_id', () => {
     describe('GET', () => {
         it('status:200, responds with the correct article object', () => {
+            const idToUse = 2
             return request(app)
-                .get('/api/articles/1')
+                .get(`/api/articles/${idToUse}`)
                 .expect(200)
                 .then(({ body }) => {
                     const { article } = body
-                    expect(article).toEqual({
-                        article_id: 1,
-                        title: 'Living in the shadow of a great man',
-                        body: 'I find this existence challenging',
-                        votes: 100,
-                        topic: 'mitch',
-                        author: 'butter_bridge',
-                        created_at: '2020-07-09T20:11:00.000Z',
-                        comment_count: 11,
-                    })
-                })
-        })
-        it('status:200, responds with a second correct article object', () => {
-            const idToUse = '2'
-            return request(app)
-                .get(`/api/articles/2`)
-                .expect(200)
-                .then(({ body }) => {
-                    const { article } = body
-                    expect(article).toEqual({
-                        article_id: 2,
-                        title: 'Sony Vaio; or, The Laptop',
-                        body: 'Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.',
-                        votes: 0,
-                        topic: 'mitch',
-                        author: 'icellusedkars',
-                        created_at: '2020-10-16T05:03:00.000Z',
-                        comment_count: 0,
-                    })
+                    expect(article).toEqual(
+                        expect.objectContaining({
+                            article_id: idToUse,
+                            title: expect.any(String),
+                            body: expect.any(String),
+                            votes: expect.any(Number),
+                            topic: expect.any(String),
+                            author: expect.any(String),
+                            created_at: expect.stringMatching(validTimestamp),
+                            comment_count: expect.any(Number),
+                        })
+                    )
                 })
         })
         it('status:400, responds with bad request when passed with invalid ID', () => {
@@ -114,24 +99,44 @@ describe('/api/articles/:article_id', () => {
             const reqToUse = {
                 inc_votes: incByAmount,
             }
+            let responseToCompare
+
             return request(app)
-                .patch(`/api/articles/${idToUse}`)
-                .send(reqToUse)
-                .expect(202)
+                .get(`/api/articles/${idToUse}`)
+                .expect(200)
                 .then(({ body }) => {
-                    const { article } = body
-                    expect(article).toEqual(
-                        expect.objectContaining({
-                            article_id: 1,
-                            title: 'Living in the shadow of a great man',
-                            body: 'I find this existence challenging',
-                            topic: 'mitch',
-                            author: 'butter_bridge',
-                            created_at: '2020-07-09T20:11:00.000Z',
-                            // comment_count: expect.toEqual(11 + incByAmount),
+                    responseToCompare = {...body}
+                    responseToCompare.article.votes += 1
+                    return
+                })
+                .then(() => {
+                    return request(app)
+                        .patch(`/api/articles/${idToUse}`)
+                        .send(reqToUse)
+                        .expect(202)
+                        .then(({ body }) => {
+                            expect(body).toEqual(responseToCompare)
                         })
-                    )
-                    expect(article.votes).toEqual(100 + incByAmount)
+                })
+        })
+        it.skip('status:400, responds with bad request when passed with invalid ID', () => {
+            const idToUse = 'notAnId'
+            return request(app)
+                .get(`/api/articles/${idToUse}`)
+                .expect(400)
+                .then(({ body }) => {
+                    const { msg } = body
+                    expect(msg).toEqual(`Invalid ID: ${idToUse}`)
+                })
+        })
+        it.skip('status:404, responds with not found when passed ID does not exist on database', () => {
+            const idToUse = '9000'
+            return request(app)
+                .get(`/api/articles/${idToUse}`)
+                .expect(404)
+                .then(({ body }) => {
+                    const { msg } = body
+                    expect(msg).toEqual(`ID not found: ${idToUse}`)
                 })
         })
     })
--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -17,9 +17,11 @@ exports.patchArticleVotes = ({ params, body }, res, next) => {
     const id = params.id
     const votes = body.inc_votes
     return updateArticleVotes(id, votes)
-        .then(({ rows }) => {
-            // rows[0].comment_count = parseInt(rows[0].comment_count)
-            return res.status(202).send({ article: rows[0] })
+        .then(() => {
+            return selectArticleById(id).then(({ rows }) => {
+                rows[0].comment_count = parseInt(rows[0].comment_count)
+                return res.status(202).send({ article: rows[0] })
+            })
         })
         .catch(next)
 }
